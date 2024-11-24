6b9a48fa0c55ee44442f220a830075f0406a6361
HanJongKyun <hjk326333@gmail.com>
2024-11-24 21:24:20 +0900

    로그인 / 회원가입 복구

--- app/src/main/java/com/example/ph_k/RegisterActivity.java
+++ app/src/main/java/com/example/ph_k/RegisterActivity.java
@@ -1,50 +1,78 @@
 package com.example.ph_k;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText usernameEditText, passwordEditText;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        usernameEditText = findViewById(R.id.usernameEditText);
-        passwordEditText = findViewById(R.id.passwordEditText);
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        registerButton = findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
     }
 
-    public void onRegisterClick(View view) {
+    private void register() {
         String username = usernameEditText.getText().toString();
         String password = passwordEditText.getText().toString();
 
-        RegisterRequest registerRequest = new RegisterRequest(username, password);
-        ApiService apiService = RetrofitClient.getRetrofitInstance().create(ApiService.class);
+        String url = "http://192.168.200.114:5000/register";  // 서버 URL
 
-        apiService.registerUser(registerRequest).enqueue(new Callback<RegisterResponse>() {
-            @Override
-            public void onResponse(Call<RegisterResponse> call, Response<RegisterResponse> response) {
-                if (response.isSuccessful()) {
-                    Toast.makeText(RegisterActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(RegisterActivity.this, "회원가입 실패", Toast.LENGTH_SHORT).show();
-                }
-            }
+        // JSON 객체 생성
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams.put("username", username);
+            jsonParams.put("password", password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonParams,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // 서버에서 반환된 응답 처리
+                        Toast.makeText(RegisterActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                        finish();  // 회원가입 성공 후 로그인 화면으로 돌아가기
+                    }
+                },
+                error -> Toast.makeText(RegisterActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show()) {
             @Override
-            public void onFailure(Call<RegisterResponse> call, Throwable t) {
-                Log.e("RegisterActivity", "Error: " + t.getMessage());
-                Toast.makeText(RegisterActivity.this, "서버 연결 실패", Toast.LENGTH_SHORT).show();
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                return headers;
             }
-        });
+        };
+
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
     }
 }
